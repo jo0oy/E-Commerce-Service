44e6e8b18e6873130261d78fcceeaefaaad349c1
jo0oy <rkfkrh07@naver.com>
2023-03-08 23:28:49 +0900

    [feature] MemberEntity 생성 및 구현
    - MemberEntity 기본 뼈대 구현

---
+++ src/main/java/com/example/ecommerce/member/MemberEntity.java
@@ -0,0 +1,65 @@
+package com.example.ecommerce.member;
+
+import com.example.ecommerce.common.exception.InvalidParamException;
+import lombok.*;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "members")
+@Entity
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 50, nullable = false, updatable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 60, nullable = false)
+    private String phoneNumber;
+
+    private Long membershipId;
+
+    private Role role;
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Role {
+        ROLE_USER("회원"),
+        ROLE_PARTNER("파트너사"),
+        ROLE_ADMIN("관리자");
+        private final String description;
+    }
+
+    @Builder
+    private MemberEntity(String username,
+                         String email,
+                         String password,
+                         String phoneNumber,
+                         Long membershipId,
+                         Role role) {
+
+        if (!StringUtils.hasText(username)) throw new InvalidParamException("empty username");
+        if (!StringUtils.hasText(email)) throw new InvalidParamException("empty email");
+        if (!StringUtils.hasText(password)) throw new InvalidParamException("empty password");
+        if (!StringUtils.hasText(phoneNumber)) throw new InvalidParamException("empty phoneNumber");
+
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.membershipId = membershipId;
+        this.role = (Objects.isNull(role)) ? Role.ROLE_USER : role;
+    }
+}
