3ed9e90642f3efbefe309da7724ff85b981e5055
jo0oy <rkfkrh07@naver.com>
2023-03-09 16:07:44 +0900

    [feature] 멤버십 엔티티 생성
    - 회원 생성 주기와 생성 주기가 같기 때문에 회원 애그리거트 하위에 멤버십 도메인 구현
    - 회원 생성시 초기 멤버십의 데이터는 모두 동일하게 생성되기 때문에 정적 메서드 create() 작성함

---
+++ src/main/java/com/example/ecommerce/domain/member/membership/MembershipEntity.java
@@ -0,0 +1,82 @@
+package com.example.ecommerce.domain.member.membership;
+
+import com.example.ecommerce.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "memberships")
+@Entity
+public class MembershipEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "membership_id")
+    private Long id;
+
+    private Integer totalSpending;
+
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @Getter
+    public enum Grade {
+        SILVER(0, 200000),
+        GOLD(200000, 400000),
+        VIP(400000);
+
+        private int goe;
+        private int lt;
+
+        Grade(int goe) {
+            this.goe = goe;
+        }
+
+        Grade(int goe, int lt) {
+            this(goe);
+            this.lt = lt;
+        }
+    }
+
+    private boolean isDeleted;
+
+    private LocalDateTime deletedAt;
+
+    @Builder
+    private MembershipEntity(Integer totalSpending,
+                             Grade grade) {
+
+        this.totalSpending = (Objects.isNull(totalSpending) || totalSpending < 0) ? 0 : totalSpending;
+        this.grade = (Objects.isNull(grade)) ? Grade.SILVER : grade;
+        this.isDeleted = false;
+        this.deletedAt = null;
+    }
+
+    // 멤버십 초기 생성 메서드
+    public static MembershipEntity create() {
+        return MembershipEntity.builder()
+                .totalSpending(0)
+                .grade(Grade.SILVER)
+                .build();
+    }
+
+    public void addTotalSpending(int spending) {
+        this.totalSpending += spending;
+    }
+
+    public void minusTotalSpending(int spending) {
+        this.totalSpending -= spending;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+}
