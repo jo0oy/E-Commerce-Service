209b58698de2701b83640e6ce5e25bc151dd45e7
jo0oy <rkfkrh07@naver.com>
2023-03-09 16:13:42 +0900

    [feature] 멤버십 비즈니스 로직 구현
    - 모든 멤버십을 업데이트하는 로직 구현

---
+++ src/main/java/com/example/ecommerce/domain/member/membership/MembershipService.java
@@ -0,0 +1,30 @@
+package com.example.ecommerce.domain.member.membership;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MembershipService {
+
+    private final MembershipUpdateExecutor membershipUpdateExecutor;
+
+    @Transactional
+    public void updateMemberships() {
+
+        var silver = MembershipEntity.Grade.SILVER;
+        var gold = MembershipEntity.Grade.GOLD;
+        var vip = MembershipEntity.Grade.VIP;
+
+        // 각 등급별로 업데이트
+        membershipUpdateExecutor.bulkUpdateGrade(silver.getGoe(), silver.getLt(), silver);
+        membershipUpdateExecutor.bulkUpdateGrade(gold.getGoe(), gold.getLt(), gold);
+        membershipUpdateExecutor.bulkUpdateGrade(vip.getGoe(), vip);
+
+        // 총 지출액 초기화
+        membershipUpdateExecutor.bulkResetTotalSpending();
+    }
+}
