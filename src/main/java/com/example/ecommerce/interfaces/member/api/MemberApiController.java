3d6564dcef6badbcffe3ce91d35c8238b9791f2b
jo0oy <rkfkrh07@naver.com>
2023-10-10 23:49:41 +0900

    MemberApiController: 회원 등록/수정/조회/삭제 API 구현

---
+++ src/main/java/com/example/ecommerce/interfaces/member/api/MemberApiController.java
@@ -0,0 +1,63 @@
+package com.example.ecommerce.interfaces.member.api;
+
+import com.example.ecommerce.application.member.MemberFacade;
+import com.example.ecommerce.common.utils.ApiUtils;
+import com.example.ecommerce.interfaces.member.dto.MemberDtoMapper;
+import com.example.ecommerce.interfaces.member.dto.request.MemberInfoUpdateRequestDto;
+import com.example.ecommerce.interfaces.member.dto.request.MemberJoinRequestDto;
+import com.example.ecommerce.interfaces.member.dto.request.MemberPwdUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+@RestController
+public class MemberApiController {
+
+    private final MemberFacade memberFacade;
+    private final MemberDtoMapper memberDtoMapper;
+
+    @PostMapping("")
+    public ResponseEntity<?> join(@Valid @RequestBody MemberJoinRequestDto requestDto) {
+        var data = memberFacade.join(memberDtoMapper.of(requestDto));
+
+        return ResponseEntity
+                .created(URI.create("/api/v1/members"))
+                .body(ApiUtils.success(memberDtoMapper.of(data)));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<?> update(@Valid @RequestBody MemberInfoUpdateRequestDto requestDto) {
+
+        memberFacade.updateInfo(requestDto.memberId(), memberDtoMapper.of(requestDto));
+
+        return ResponseEntity.ok(ApiUtils.success("회원 정보를 정상적으로 업데이트 완료했습니다."));
+    }
+
+    @PutMapping("/{memberId}")
+    public ResponseEntity<?> updatePassword(@Valid @RequestBody MemberPwdUpdateRequestDto requestDto,
+                                            @PathVariable("memberId") Long memberId) {
+
+        memberFacade.updatePassword(memberId, memberDtoMapper.of(requestDto));
+
+        return ResponseEntity.ok(ApiUtils.success("비밀번호를 정상적으로 변경 완료했습니다."));
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<?> delete(@PathVariable("memberId") Long memberId) {
+        memberFacade.delete(memberId);
+
+        return ResponseEntity.ok(ApiUtils.success("정상적으로 회원을 삭제했습니다."));
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<?> getMember(@PathVariable("memberId") Long memberId) {
+        var data = memberDtoMapper.of(memberFacade.getMember(memberId));
+
+        return ResponseEntity.ok(ApiUtils.success(data));
+    }
+}
